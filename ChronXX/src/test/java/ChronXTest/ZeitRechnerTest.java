2ef75622e1edb21852ea9b08d5c5ecb2c6a1d680
IsaakThaddaeus <66296094+IsaakThaddaeus@users.noreply.github.com>
2022-09-24 12:53:52 +0200

    Add files via upload

---
+++ ChronXX/src/test/java/ChronXTest/ZeitRechnerTest.java
@@ -0,0 +1,19 @@
+package ChronXTest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
+import ChronXProgramm.ZeitRechner;
+public class ZeitRechnerTest {
+//	@Test
+//	public void aktuelleZeitAbfrage() {
+//		 LocalDateTime date = LocalDateTime.now();
+//			ZeitRechner zr = new ZeitRechner();
+//		String ergebnis = zr.aktuelleZeit();
+//		assertEquals(date.getYear()+"."+date.getMonthValue()+"."+date.getDayOfMonth()+"-"+date.getHour()+":"+date.getMinute(), ergebnis);
+//	}
+
+}
